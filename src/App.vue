7425e3df6774516dce95cfcf5542cfca02fab888
anas24eng <62037064+anas24eng@users.noreply.github.com>
2021-06-14 16:54:52 +0300

    Happy coding

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+      <Navbar/>
+      <Spinner/>
+    <v-content>
+        <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+import Spinner from '@/components/Spinner'
+export default {
+  name: 'App',
+  components: {
+  Navbar,
+  Spinner
+
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,28 @@
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
+
+
+Vue.use(Vuetify);
+
+
+export default new Vuetify({
+    icons: {
+      iconfont: 'md' || 'fa'
+    },
+    theme: {
+    
+        
+      themes: {
+        light: {
+          backgrund: colors.grey.lighten2,
+        },
+        dark: {
+            
+          backgrund: colors.shades.white,
+        }
+      }
+    }
+  })
